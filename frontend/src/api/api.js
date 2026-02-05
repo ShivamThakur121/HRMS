f4134a9bb3c6a679a3556d0b85b2a51f637ca718
Shivam Thakur <shivamkumr.chk1234@gmail.com>
2026-02-05 10:43:53 +0530

    Update API base URL for axios instance

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.http://hrms-1qt8.onrender.com/api
+  baseURL: "https://hrms-backend-i7tw.onrender.com/api"
 });
 
 export default api;
